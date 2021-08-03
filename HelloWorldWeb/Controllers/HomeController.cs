1f908c5caa2be74e2141dbb9e8f87290a4173415
Leon Campean <leoncampean90@gmail.com>
2021-08-03 11:53:09 +0300

    Adding Members in Team-Script

--- HelloWorldWeb/Controllers/HomeController.cs
+++ HelloWorldWeb/Controllers/HomeController.cs
@@ -12,20 +12,32 @@ namespace HelloWorldWeb.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+        private readonly TeamInfo teamInfo;
 
-		public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
+			teamInfo = new TeamInfo();
+			teamInfo.Name = "Team3";
+			teamInfo.TeamMembers = new List<string>();
+			teamInfo.TeamMembers.Add("Radu");
+			teamInfo.TeamMembers.Add("Leon");
+			teamInfo.TeamMembers.Add("Teona");
+			teamInfo.TeamMembers.Add("George");
+			teamInfo.TeamMembers.Add("Dragos");
+			teamInfo.TeamMembers.Add("Claudia");
+			
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+			return View(teamInfo);
+			;
 		}
 
 		public IActionResult Privacy()
 		{
-			return View();
+			return View(teamInfo);
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
