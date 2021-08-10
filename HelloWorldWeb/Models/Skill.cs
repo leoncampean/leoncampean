87f7cb0e236ddb8caf7bbc45cd1a56c96859b1af
Leon Campean <leoncampean90@gmail.com>
2021-08-10 09:31:54 +0300

    Skill Model added

--- HelloWorldWeb/Models/Skill.cs
+++ HelloWorldWeb/Models/Skill.cs
@@ -1,17 +1,14 @@
 using System.Runtime.InteropServices;
-
-// In SDK-style projects such as this one, several assembly attributes that were historically
-// defined in this file are now automatically added during build and populated with
-// values defined in project properties. For details of which attributes are included
-// and how to customise this process see: https://aka.ms/assembly-info-properties
+using System;
+using System.Collections.Generic;
 
 
-// Setting ComVisible to false makes the types in this assembly not visible to COM
-// components.  If you need to access a type in this assembly from COM, set the ComVisible
-// attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM.
-
-[assembly: Guid("35258ba1-30f2-4033-b6ad-0c7f4351d9cd")]
+namespace  HelloWorldWeb.Models
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Skilluri { get; set; }
+    }
+}
