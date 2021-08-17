91664f9f91dd351b4f95857028aa8ed1af469909
Leon Campean <leoncampean90@gmail.com>
2021-08-17 14:45:28 +0300

    Recreating the EF branch

---
+++ HelloWorldWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using HelloWorldWebApp.Models;
+
+namespace HelloWorldWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<HelloWorldWebApp.Models.Skill> Skill { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # My first app
 Sample text here
 #using ef here
+
+
+create new db in postgress HelloWorld
